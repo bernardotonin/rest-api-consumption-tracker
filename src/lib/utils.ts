f4af54fe29287f456d763c578fb17d6717e427fc
bernardotonin <btprates2004@gmail.com>
2024-08-27 16:03:42 -0600

    feat: create middleware function to validate upload route body

---
+++ src/lib/utils.ts
@@ -0,0 +1,28 @@
+import { Request, Response, NextFunction } from 'express';
+import { getZodErrorMessage, UploadSchema } from './schemas';
+
+export const ImageExtensionMap: Record<string, string> = {
+  '/': 'image/jpeg',
+  i: 'image/png',
+  U: 'image/webp'
+};
+
+export const ValidateUploadData = (req: Request, res: Response, next: NextFunction) => {
+  const validatedBody = UploadSchema.safeParse(req.body);
+
+  if (!validatedBody.success) {
+    return res.status(400).json({
+      error_code: 'INVALID_DATA',
+      error_description: getZodErrorMessage(validatedBody.error)
+    });
+  }
+
+  if(ImageExtensionMap[validatedBody.data.image[0]] === undefined){
+    return res.status(400).json({
+      error_code: 'INVALID_DATA',
+      error_description: 'Apenas imagems em formato JPEG, PNG ou WEBP são aceitas.'
+    });
+  }
+
+  next();
+};
