da4ece5413629b6715488add9b181c28d8df1976
bernardotonin <btprates2004@gmail.com>
2024-08-27 22:23:46 -0600

    feat: create ConfirmSchema and ListParamsSchema

--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -8,11 +8,33 @@ export const getZodErrorMessage = (error: z.ZodError): string[] => {
 };
 
 export const UploadSchema = z.object({
-  image: z.string({ required_error: 'A imagem é obrigatória' }).refine(Base64.isValid, 'Base64 inválido!'),
-  customer_code: z.string({ required_error: 'Código do cliente é obrigatório' }),
+  image: z
+    .string({ required_error: 'A imagem é obrigatória' })
+    .refine(Base64.isValid, 'Base64 inválido!'),
+  customer_code: z
+    .string({ required_error: 'Código do cliente é obrigatório' })
+    .min(1, 'Codigo do Cliente não deve ser vazio!'),
   measure_datetime: z
     .string({ required_error: 'Data é obrigatória!' })
     // offsets podem ser adicionados com offset: true nesse objeto ⬇️
     .datetime({ message: 'Data deve ser no formato ISO 8601' }),
   measure_type: z.enum(['WATER', 'GAS'], { message: 'Tipo deve ser WATER ou GAS' })
+});
+
+export const ConfirmSchema = z.object({
+  measure_uuid: z.string({ required_error: 'UUID da medição é obrigatório!' }),
+  confirmed_value: z.number({ required_error: 'Valor é obrigatório!' })
+});
+
+export const ListParamsSchema = z.object({
+  customer_code: z
+    .string({ required_error: 'Código do cliente é obrigatório' })
+    .min(1, 'Codigo do Cliente não deve ser vazio!'),
+  measure_type: z
+    .string()
+    .refine((value) => {
+      const upperCaseValue = value.toUpperCase();
+      return ['WATER', 'GAS'].includes(upperCaseValue);
+    }, 'Tipo deve ser WATER ou GAS')
+    .optional()
 });
