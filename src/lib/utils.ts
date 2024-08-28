345bf17fcdfbb9f1fb6a7e38ce8fc16649bde80b
bernardotonin <btprates2004@gmail.com>
2024-08-27 20:00:31 -0600

    feat: create utility function to write base64 derived files

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,10 +1,38 @@
 import { Request, Response, NextFunction } from 'express';
 import { getZodErrorMessage, UploadSchema } from './schemas';
+import crypto from 'crypto';
+import fs from 'fs';
+import path from 'path';
 
-export const ImageExtensionRecord: Record<string, string> = {
+export const ImageTypeRecord: Record<string, string> = {
   '/': 'image/jpeg',
   i: 'image/png',
   U: 'image/webp'
+};
+
+export const ImageExtensionRecord: Record<string, string> = {
+  '/': 'jpeg',
+  i: 'png',
+  U: 'webp'
+};
+
+const GenerateUniqueImageName = (base64string: string) => {
+  return crypto.createHash('md5').update(base64string).digest('hex');
+};
+
+export const WriteBase64Image = (base64string: string) => {
+  const buffer = Buffer.from(base64string, 'base64');
+
+  const fileName = `${GenerateUniqueImageName(base64string)}.${ImageExtensionRecord[base64string[0]]}`
+
+  const filePath = path.join(
+    __dirname,
+    '../../public/images',
+    fileName
+  );
+
+  fs.writeFileSync(filePath, buffer);
+  return fileName;
 };
 
 export const ValidateUploadData = (req: Request, res: Response, next: NextFunction) => {
@@ -17,7 +45,7 @@ export const ValidateUploadData = (req: Request, res: Response, next: NextFuncti
     });
   }
 
-  if(ImageExtensionRecord[validatedBody.data.image[0]] === undefined){
+  if (ImageTypeRecord[validatedBody.data.image[0]] === undefined) {
     return res.status(400).json({
       error_code: 'INVALID_DATA',
       error_description: 'Apenas imagems em formato JPEG, PNG ou WEBP são aceitas.'
